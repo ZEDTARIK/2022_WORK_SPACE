949aee955719cd6691364e64bbdd917b58d1dd17
unknown <ettarak.zouhair@gmail.com>
2021-12-24 10:24:09 +0100

    Event , click end mouseover

--- VUE_FORMATION/app.js
+++ VUE_FORMATION/app.js
@@ -1,14 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Shoes",
       image: "assets/socks_green.jpg",
       inventory: 45,
       details: ["50% cotton", "30% polyester", "20% wool"],
       variantes: [
-        { id: 2234, color: "Green" },
-        { id: 2235, color: "Blue" },
+        { id: 2234, color: "Green", image: "./assets/socks_green.jpg" },
+        { id: 2235, color: "Blue", image: "./assets/socks_blue.jpg" },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
