885e1a57658331fb416dd42322c894e1bd5ae238
unknown <ettarak.zouhair@gmail.com>
2021-12-24 11:44:13 +0100

    Event Second step

--- VUE_FORMATION/app.js
+++ VUE_FORMATION/app.js
@@ -3,12 +3,13 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Shoes",
+      inStock: true,
       image: "assets/socks_green.jpg",
       inventory: 45,
       details: ["50% cotton", "30% polyester", "20% wool"],
       variantes: [
-        { id: 2234, color: "Green", image: "./assets/socks_green.jpg" },
-        { id: 2235, color: "Blue", image: "./assets/socks_blue.jpg" },
+        { 'id': 2234, 'color': "green", 'image': "./assets/socks_green.jpg", 'inStock': true },
+        { 'id': 2235, 'color': "blue", 'image': "./assets/socks_blue.jpg", 'inStock': false },
       ],
     };
   },
